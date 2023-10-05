550eeef645d9c70c5ce2c8ecaebb74bc788f5a40
Jamshid Sadiqi <jamshidsadiqi25@gmail.com>
2023-10-05 14:32:12 +0300

    Add Main.java

---
+++ datefunction/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        CalenderClass calenderClass = new CalenderClass();
+        System.out.println(calenderClass.findTheDay(6,14,2046));
+    }
+}
